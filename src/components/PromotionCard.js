782525689f1dbd250d21618d633c0bbdc3676fb7
AnshuGupta23 <anshu.gupta@cloudkaptan.com>
2026-02-05 10:52:05 +0530

    hi

--- src/components/PromotionCard.js
+++ src/components/PromotionCard.js
@@ -1,6 +1,6 @@
 const PromotionCard = (props) => {
   return (
-    <div className="border-2 h-full cursor-pointer">
+    <div className="border-2 h-full cursor-pointer rounded-4xl">
       <h1>{props.title}</h1>
       <h1>{props.description}</h1>
     </div>
